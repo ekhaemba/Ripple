f8997a2a78ce7500bca59185c9dedcbe3c218c61
Syncla <vvazir@udel.edu>
2018-05-05 15:27:34 -0400

    updoots to my files
    Signed-off-by: Syncla <vvazir@udel.edu>

--- testScrapper.py
+++ testScrapper.py
@@ -1,10 +1,13 @@
 from configparser import ConfigParser
 import unittest
+import selenium
+from bs4 import BeautifulSoup as bsoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 class PythonOrgSearch(unittest.TestCase):
     user = ""
@@ -22,20 +25,57 @@ class PythonOrgSearch(unittest.TestCase):
     def test_search_in_python_org(self):
         driver = self.driver
         url = self.url
-        #driver.get(self.url)
-        ready = WebDriverWait(url,10).until(EC.presence_of_element_located((By.ID, 'loginwindow-1010')))
-        #time.sleep(13)
+        print(url)
+        driver.get(self.url)
+        time.sleep(4)
         #self.assertIn("emdat_db", driver.title)
         user = driver.find_element_by_id("textfield-1012-inputEl")
-        user.send_keys("")
+        user.send_keys(self.user)
         password = driver.find_element_by_id("textfield-1013-inputEl")
-        password.send_keys("")
-        log = driver.find_element_by_id("button-1017-btnInnerEl")
-        print("Clicked")
-        log.click()
-        #assert "No results found." not in driver.page_source
-        page = driver.page_source
-        print(page)
+        password.send_keys(self.password)
+        login = driver.find_element_by_id("button-1017-btnInnerEl")
+        login.click()
+        try:
+            page = WebDriverWait(driver,10)
+            profiles = page.until(EC.presence_of_element_located((By.XPATH,'//*[@id="button-1026-btnInnerEl"]')))
+        except TimeoutError:
+            print("Took to long to load page")
+        profiles.click()
+        time.sleep(1)
+        # Country selection
+        countryRadio = driver.find_element_by_id("radiofield-1055-inputEl")
+        countryRadio.click()
+        countryTop = driver.find_element_by_xpath('//*[@id="boundlist-1079-listEl"]/li[1]')
+        countryBot = driver.find_element_by_xpath('//*[@id="boundlist-1079-listEl"]/li[230]')
+        addBtn = driver.find_element_by_xpath('//*[@id="button-1085-btnIconEl"]')
+        ActionChains(driver).key_down(Keys.SHIFT).click(countryTop).click(countryBot).key_up(Keys.SHIFT).perform()
+        addBtn.click()
+        # Disaster selection
+        time.sleep(20)
+        climate = driver.find_element_by_xpath('//*[@id="ext-element-48"]/div/input')
+        aliens = driver.find_element_by_xpath('//*[@id="ext-element-44"]/div/input')
+        geo = driver.find_element_by_xpath('//*[@id="ext-element-41"]/div/input')
+        hydro = driver.find_element_by_xpath('//*[@id="treeview-1091-record-387"]/tbody/tr/td/div/input')
+        meteor = driver.find_element_by_xpath('//*[@id="treeview-1091-record-388"]/tbody/tr/td/div/input')
+        search = driver.find_element_by_xpath('//*[@id="button-1364-btnInnerEl"]')
+        death = driver.find_element_by_xpath('//*[@id="boundlist-1353-listEl"]/li[3]')
+        damage = driver.find_element_by_xpath('//*[@id="boundlist-1353-listEl"]/li[5]')
+        searchAdd = driver.find_element_by_xpath('//*[@id="button-1359-btnIconEl"]')
+        
+        climate.click()
+        aliens.click()
+        geo.click()
+        hydro.click()
+        meteor.click()
+        death.click()
+        damage.click()
+        searchAdd.click()
+        search.click()
+        time.sleep(5)
+        html = driver.page_source
+        soup = bsoup(html)
+
+
         
     def tearDown(self):
         self.driver.close()
