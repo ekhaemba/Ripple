410d5dd7dc7ba022897704c03d3d51159ab91ea8
Jason Reynolds <jkr@udel.edu>
2018-05-06 14:19:15 -0400

    updated dis model

--- disasterModels/model_v0.py
+++ disasterModels/model_v0.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
 
 #importing dataset
 dataset = pd.read_csv('testData.csv')
@@ -10,3 +13,22 @@ output = dataset.iloc[:,-1].values
 
 #splitting the dataset into training and test
 inputTrain, inputTest, outputTrain, outputTest = train_test_split(inputs,output,test_size = 0.2)
+
+#
+#regressor = SVR(kernel = 'rbf')
+#regressor.fit(inputs,output)
+#output_pred = regressor.predict(6.5)
+
+regressor = LinearRegression()
+regressor.fit(inputTrain,outputTrain)
+
+output_pred = regressor.predict(inputTest)
+
+
+plt.scatter(inputs,output,color = 'red')
+plt.plot(inputs,regressor.predict(inputs),color = 'blue')
+plt.title('Truth or Bluff')
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.show()
+
